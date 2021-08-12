ec89f7adc4c32268acc82dbccb236abc1ad67d7a
grandeto <grandeto.com@gmail.com>
2021-08-13 00:46:22 +0300

    handle multiple urls

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func scrapePrices(prices chan string, wg *sync.WaitGroup) {
+func scrapePrices(url string, tag string, prices chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	// Request the HTML page.
-	res, err := http.Get("https://sopharmacy.bg/bg/product/000000000030011088")
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,18 +27,33 @@ func scrapePrices(prices chan string, wg *sync.WaitGroup) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Find the review items
-	prices <- doc.Find(".price.price--inline.price--l").Text()
-
+	prices <- doc.Find(tag).Text() + " - " + url
 }
 
 func main() {
-	var wg sync.WaitGroup
-	prices := make(chan string)
+	urls := map[string]string{
+		"https://sopharmacy.bg/bg/product/000000000030011088": ".price.price--inline.price--l",
+		"https://bemore.shop/bg/2-br-collanol-s-25-otst-pka": ".product-info-price .price",
+		"https://bemore.shop/bg/collanol-10": ".product-info-price .price",
+		"https://subra.bg/bg/hranitelni-dobavki/vitaslim-collanol-x-20-caps.html": "#sec_discounted_price_12778",
+		"https://remedium.bg/collanol-intakten-kolagen-i-kurkumin-za-zdravi-kosti-i-stavi-h20-kapsuli-148074/p": ".Price__PriceLabel-sc-14hy5o8-1",
+		"https://befit.bg/collanol-kolagen-i-kurkumin-za-zdravi-stavi-i-kosti-20-kaps": ".price-box .price",
+		"https://www.aptekadetelina.bg/collanol-kolanol-680-mg-20-kapsuli?manufacturer_id=575" : "#ProductPriceSystem_DAuHUM6x .price",
+	}
 
-	go scrapePrices(prices, &wg)
-	res := <-prices
-	fmt.Printf("Price: %s\n", res)
+	var wg sync.WaitGroup
+
+	prices := make(chan string, len(urls))
+
+	for url, tag := range urls {
+		wg.Add(1)
+		go scrapePrices(url, tag, prices, &wg)
+	}
+
 	wg.Wait()
+	close(prices)
+
+	for price := range prices {
+		fmt.Printf("%s\n\n", price)
+	}
 }
